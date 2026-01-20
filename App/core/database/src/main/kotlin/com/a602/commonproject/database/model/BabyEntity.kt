28003ca440446f9678c1b992f077da86d12ce9c7
김경호 <kkh9331@naver.com>
2026-01-21 02:11:51 +0900

    [App] feat: Room DB 인티티 추가 완료

---
+++ App/core/database/src/main/kotlin/com/a602/commonproject/database/model/BabyEntity.kt
@@ -0,0 +1,15 @@
+package com.a602.commonproject.database.model
+
+import android.net.Uri
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "babies")
+data class BabyEntity(
+    @PrimaryKey val babyId : String,
+    val name : String,
+    val birthDate : Long,
+    val profileUrl: String,           // 서버 URL (없으면 기본 아이콘 표시)
+
+    val gender: String = "U"         // 선택 사항, M(남), F(여), U(미정) - 테마 색상 결정용
+)
