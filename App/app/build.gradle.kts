099b7ef7d0c962a527bf55527b133add998f0d4d
김경호 <kkh9331@naver.com>
2026-01-22 17:49:15 +0900

    [App] feat : sync 모듈 연결

--- App/app/build.gradle.kts
+++ App/app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
 }
 
 dependencies {
+    implementation(projects.sync)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
