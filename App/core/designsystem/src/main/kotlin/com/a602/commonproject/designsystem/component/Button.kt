9a936ba20a7f890b7711c0378772a48448168af6
KIMSUJIN453 <2014ksj05@naver.com>
2026-01-23 17:42:25 +0900

    [App] feat : 버튼 컴포넌트 구현 중

--- App/core/designsystem/src/main/kotlin/com/a602/commonproject/designsystem/component/Button.kt
+++ App/core/designsystem/src/main/kotlin/com/a602/commonproject/designsystem/component/Button.kt
@@ -1,2 +1,51 @@
 package com.a602.commonproject.designsystem.component
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.a602.commonproject.designsystem.theme.main
+
+
+@Composable
+fun CommonButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = main,
+        ),
+        contentPadding = contentPadding,
+        content = content,
+    )
+}
+
+
+
+@Preview
+@Composable
+fun CommonButtonPreview() {
+    MaterialTheme {
+        Surface(modifier = Modifier.size(150.dp, 50.dp)) {
+            CommonButton(
+                onClick = {},
+                content = { Text("button") }
+            )
+        }
+    }
+}
