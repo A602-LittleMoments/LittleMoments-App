f6c3472f11407d852197a3de3b105d0ea8dae8d2
서지원 <jiwonseo199@gmail.com>
2026-01-29 16:21:10 +0900

    [APP] fix : Polaroid, Gallery,MediaDetail

--- App/core/ui/src/main/kotlin/com/a602/coommonproject/ui/Media_Detail.kt
+++ App/core/ui/src/main/kotlin/com/a602/coommonproject/ui/Media_Detail.kt
@@ -1,39 +1,18 @@
 package com.a602.coommonproject.ui
 
-import android.graphics.Bitmap
-import androidx.compose.foundation.background
+import Polaroid
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Pause
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.a602.commonproject.designsystem.component.IconActionBar
-import com.a602.commonproject.designsystem.component.LMTopAppBar
-import com.a602.commonproject.designsystem.component.Polaroid
 import com.a602.commonproject.designsystem.theme.background
-import com.a602.commonproject.designsystem.theme.color1
-import androidx.compose.foundation.clickable
-import androidx.compose.ui.unit.Dp
 import com.a602.commonproject.model.data.SharedMedia
 
 
