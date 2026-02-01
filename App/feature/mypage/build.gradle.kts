3e903a58f032a332257560f55faff0f016d5a2d6
조현서 <814andy@naver.com>
2026-02-01 10:19:45 +0900

    [App] fix:아이수정기능수정

--- App/feature/mypage/build.gradle.kts
+++ App/feature/mypage/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.commonproject.android.feature.api)
     alias(libs.plugins.commonproject.android.feature.impl)
     alias(libs.plugins.commonproject.android.library.compose)
+    id("kotlin-parcelize") // 데이터를 화면 간에 전달하기 위한 Parcelize 플러그인을 직접 추가합니다.
 }
 
 android {
@@ -18,4 +19,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0") // 버전은 프로젝트에 맞게 조절
 
+    implementation(libs.androidx.activity.compose)
+    implementation("io.coil-kt:coil-compose:2.7.0") // 라이브러리 실제 이름으로 수정
 }
